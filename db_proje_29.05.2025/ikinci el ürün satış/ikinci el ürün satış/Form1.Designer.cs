9c18ce6b36e500f352531f5fec8de94e692e358f
burakderee <burakdere2244@gmail.com>
2025-05-29 20:39:33 +0300

    Add files via upload

---
+++ db_proje_29.05.2025/ikinci el ürün satış/ikinci el ürün satış/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace ikinci_el_ürün_satış
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtUrunYasi = new System.Windows.Forms.TextBox();
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.txtHasarDurumu = new System.Windows.Forms.TextBox();
+            this.txtUrunFiyati = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.txtEsyaAdi = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "txtUrunYasi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "txtHasarDurumu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "txtUrunAdi";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(405, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "cmbEsya";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(405, 95);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "cmbKullanici";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(405, 46);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(79, 16);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "txtUrunFiyati";
+            // 
+            // txtUrunYasi
+            // 
+            this.txtUrunYasi.Location = new System.Drawing.Point(178, 43);
+            this.txtUrunYasi.Name = "txtUrunYasi";
+            this.txtUrunYasi.Size = new System.Drawing.Size(100, 22);
+            this.txtUrunYasi.TabIndex = 11;
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.Location = new System.Drawing.Point(157, 144);
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.Size = new System.Drawing.Size(100, 22);
+            this.txtUrunAdi.TabIndex = 12;
+            // 
+            // txtHasarDurumu
+            // 
+            this.txtHasarDurumu.Location = new System.Drawing.Point(178, 95);
+            this.txtHasarDurumu.Name = "txtHasarDurumu";
+            this.txtHasarDurumu.Size = new System.Drawing.Size(100, 22);
+            this.txtHasarDurumu.TabIndex = 13;
+            // 
+            // txtUrunFiyati
+            // 
+            this.txtUrunFiyati.Location = new System.Drawing.Point(528, 40);
+            this.txtUrunFiyati.Name = "txtUrunFiyati";
+            this.txtUrunFiyati.Size = new System.Drawing.Size(100, 22);
+            this.txtUrunFiyati.TabIndex = 14;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(178, 212);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 15;
+            this.btnEkle.Text = "ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(291, 212);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(75, 23);
+            this.btnListele.TabIndex = 16;
+            this.btnListele.Text = "listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(412, 212);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 17;
+            this.btnSil.Text = "sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(157, 288);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(508, 150);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(528, 89);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(100, 22);
+            this.txtKullaniciAdi.TabIndex = 19;
+            // 
+            // txtEsyaAdi
+            // 
+            this.txtEsyaAdi.Location = new System.Drawing.Point(528, 141);
+            this.txtEsyaAdi.Name = "txtEsyaAdi";
+            this.txtEsyaAdi.Size = new System.Drawing.Size(100, 22);
+            this.txtEsyaAdi.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEsyaAdi);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtUrunFiyati);
+            this.Controls.Add(this.txtHasarDurumu);
+            this.Controls.Add(this.txtUrunAdi);
+            this.Controls.Add(this.txtUrunYasi);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtUrunYasi;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.TextBox txtHasarDurumu;
+        private System.Windows.Forms.TextBox txtUrunFiyati;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.TextBox txtEsyaAdi;
+    }
+}
+
---
+++ db_proje_29.05.2025/ikinci el ürün satış/ikinci el ürün satış/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+namespace ikinci_el_ürün_satış
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void ListeleUrunler()
+        {
+            string connectionString = @"Server=DESKTOP-R0OJKTE\SQLEXPRESS;Database=second_hand_db; Trusted_Connection=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = @"SELECT u.urunID, u.urunAdi, u.urunYasi, u.hasarDurumu, u.urunFiyati,
+                         e.esyaAdi, k.kullaniciAdi
+                         FROM urunBilgi u
+                         JOIN esya e ON u.esyaID = e.esyaID
+                         JOIN kullaniciBilgileri k ON u.kullaniciID = k.kullaniciID";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Server=DESKTOP-R0OJKTE\SQLEXPRESS;Database=second_hand_db; Trusted_Connection=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            string kullaniciAdi = txtKullaniciAdi.Text.Trim();
+            string esyaAdi = txtEsyaAdi.Text.Trim();
+            int kullaniciID = -1;
+            int esyaID = -1;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    // Kullanıcı varsa al yoksa ekle
+                    SqlCommand cmdKullanici = new SqlCommand("SELECT kullaniciID FROM kullaniciBilgileri WHERE kullaniciAdi = @adi", conn);
+                    cmdKullanici.Parameters.AddWithValue("@adi", kullaniciAdi);
+                    var resultKullanici = cmdKullanici.ExecuteScalar();
+
+                    if (resultKullanici != null)
+                    {
+                        kullaniciID = Convert.ToInt32(resultKullanici);
+                    }
+                    else
+                    {
+                        SqlCommand ekleKullanici = new SqlCommand("INSERT INTO kullaniciBilgileri (kullaniciAdi) OUTPUT INSERTED.kullaniciID VALUES (@adi)", conn);
+                        ekleKullanici.Parameters.AddWithValue("@adi", kullaniciAdi);
+                        kullaniciID = (int)ekleKullanici.ExecuteScalar();
+                    }
+
+                    // Eşya varsa al yoksa ekle
+                    SqlCommand cmdEsya = new SqlCommand("SELECT esyaID FROM esya WHERE esyaAdi = @adi", conn);
+                    cmdEsya.Parameters.AddWithValue("@adi", esyaAdi);
+                    var resultEsya = cmdEsya.ExecuteScalar();
+
+                    if (resultEsya != null)
+                    {
+                        esyaID = Convert.ToInt32(resultEsya);
+                    }
+                    else
+                    {
+                        SqlCommand ekleEsya = new SqlCommand("INSERT INTO esya (esyaAdi) OUTPUT INSERTED.esyaID VALUES (@adi)", conn);
+                        ekleEsya.Parameters.AddWithValue("@adi", esyaAdi);
+                        esyaID = (int)ekleEsya.ExecuteScalar();
+                    }
+
+                    // Ürünü ekle
+                    SqlCommand insertUrun = new SqlCommand(@"INSERT INTO urunBilgi 
+                (urunAdi, urunYasi, hasarDurumu, urunFiyati, esyaID, kullaniciID) 
+                VALUES (@urunAdi, @urunYasi, @hasarDurumu, @urunFiyati, @esyaID, @kullaniciID)", conn);
+
+                    insertUrun.Parameters.AddWithValue("@urunAdi", txtUrunAdi.Text.Trim());
+                    insertUrun.Parameters.AddWithValue("@urunYasi", int.Parse(txtUrunYasi.Text));
+                    insertUrun.Parameters.AddWithValue("@hasarDurumu", txtHasarDurumu.Text.Trim());
+                    insertUrun.Parameters.AddWithValue("@urunFiyati", decimal.Parse(txtUrunFiyati.Text));
+                    insertUrun.Parameters.AddWithValue("@esyaID", esyaID);
+                    insertUrun.Parameters.AddWithValue("@kullaniciID", kullaniciID);
+
+                    insertUrun.ExecuteNonQuery();
+
+                    MessageBox.Show("Ürün başarıyla eklendi.");
+
+                    // İstersen ürün listesini yenilemek için buraya listeleme kodu da ekleyebilirsin
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            ListeleUrunler();
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Server=DESKTOP-R0OJKTE\SQLEXPRESS;Database=second_hand_db; Trusted_Connection=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int urunID = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["urunID"].Value);
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("DELETE FROM urunBilgi WHERE urunID = @id", conn);
+                    cmd.Parameters.AddWithValue("@id", urunID);
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Ürün silindi.");
+                ListeleUrunler();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen silmek için bir satır seçin.");
+            }
+        }
+    }
+}
